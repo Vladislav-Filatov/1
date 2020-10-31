e10be34cbc1d7a83da3474d30f1afdf65d36d9ad
KIT_07_206 <KIT_07_206@stud.kai.ru>
2020-10-31 19:57:38 +0300

    +jump

--- Player.gd
+++ Player.gd
@@ -12,4 +12,10 @@ func _physics_process(delta):
 	elif Input.is_action_pressed("player_right"):
 		vel.x += speed
 	move_and_slide(vel, Vector2.UP)
+	
+	vel.y = gravity*delta
+	
+	if Input.is_action_pressed("player_jump"):
+		vel.y -= jumpforce
+		
 		
