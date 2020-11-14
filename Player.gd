f919950d9565463e62e8f6f90b029c23a0e1663a
KIT_07_206 <KIT_07_206@stud.kai.ru>
2020-11-14 18:34:37 +0300

    new colision

--- Player.gd
+++ Player.gd
@@ -1,21 +1,28 @@
 extends KinematicBody2D
 
-var speed = 50
-var jumpforce = 100
+var speed = 30
+var jumpforce = 500
 var gravity = 500
-
+onready var sprite = get_node("player")
 var vel = Vector2()
+
 
 func _physics_process(delta):
 	if Input.is_action_pressed("player_left"):
 		vel.x -= speed
 	elif Input.is_action_pressed("player_right"):
 		vel.x += speed
-	move_and_slide(vel, Vector2.UP)
-	
-	vel.y = gravity*delta
-	
-	if Input.is_action_pressed("player_jump"):
+	if Input.is_action_pressed("player_jump") and is_on_floor():
 		vel.y -= jumpforce
+	vel.y += gravity*delta
+	vel = move_and_slide(vel, Vector2.UP)
+	if vel.x<0:
+		sprite.flip_h = true
+	if vel.x>0:
+		sprite.flip_h = false
+	
+	
+	
+	
 		
 		
